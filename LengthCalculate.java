a322a2509c33ede7ace473ceba176edf2cf5a838
Ashish Sahu <aajisahu@gmail.com>
2021-09-21 15:09:16 +0530

    ADD: checking two line input are same greater or smaller

--- LengthCalculate.java
+++ LengthCalculate.java
@@ -8,70 +8,74 @@ import Day8_Programs.PresentAbsent;
  
 
 abstract class Compute {
-	abstract double getInput();
-	abstract double getSecondInput();
+	abstract void getInput();  //trying to use abstract class and method to see how actully its work
+	abstract void getSecondInput();
 }
 
 public class LengthCalculate extends Compute {
 	private static final Logger Log= LogManager.getLogger(LengthCalculate.class);
 	
+	double dis, dis2;//instance variable
 	
-	double getInput(){
-		double dis;
+	//declaring body to a abstract method
+	void getInput(){ //taking first line input
+		
 		Scanner sc= new Scanner(System.in);
 		
 		Log.info("Enter x1 cordinate");
 		int x1=sc.nextInt(); 
+		
 		Log.info("Enter x2 cordinate");
 		int x2=sc.nextInt(); 
+		
 		Log.info("Enter y1 cordinate");
 		int y1=sc.nextInt(); 
+		
 		Log.info("Enter y2 cordinate");
 		int y2=sc.nextInt(); 
+		
 		dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	
 		
 		Log.info("Length of line of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
 		
-		return dis;
 	}
 	
-	 double getSecondInput() {
+	 void getSecondInput() { //taking second line input
 		
 		Scanner sc2= new Scanner(System.in);
 		
-		double dis2;
-		
 		Log.info("Enter p1 cordinate");
 		int p1=sc2.nextInt(); 
+		
 		Log.info("Enter p2 cordinate");
 		int p2=sc2.nextInt(); 
+		
 		Log.info("Enter q1 cordinate");
 		int q1=sc2.nextInt(); 
+		
 		Log.info("Enter q2 cordinate");
 		int q2=sc2.nextInt(); 
+		
 		dis2=Math.sqrt((p2-p1)*(p2-p1) + (q2-q1)*(q2-q1));	
 		
 		Log.info("Length of line of coordinates"+"("+p1+","+p1+"),"+"("+q2+","+q2+")===>"+dis2);
 		
-		return dis2;
 	}
 	
-	
-	
-	
 	public static void main(String[] args) {
+		
 		LengthCalculate obj= new LengthCalculate();
 		obj.getInput();
 		obj.getSecondInput();
 		
-		if (obj.getInput() == obj.getSecondInput())
-		{
-			
+		if (obj.dis == obj.dis2) // comparing instance variable by calling directly
 			Log.info("Both lines are equale");
 			
-		}
-		else
-			Log.info("Both lines not are equale");
+		else if (obj.dis >obj.dis2)
+			Log.info("Line one is greater than line two");
+		
+		else 
+			Log.info("Line one is smaller than line two");
 		
 	}
 	
